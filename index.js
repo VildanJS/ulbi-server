9fbf97844f35168be6faa95397f9a4f38cadc8d4
VildanJS <habvil@yandex.ru>
2024-05-26 19:48:03 +0500

    update port to 8000 and 443

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ server.use(router)
 
 
 const httpsServer = https.createServer(options, server)
-const PORT = 443
+const PORT = 8443
 // запуск сервера
 httpsServer.listen(PORT, () => {
   console.log(`server is running on ${PORT} port`)
